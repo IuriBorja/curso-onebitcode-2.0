e055369e509c9c6a63de1f7e164526a9de683656
Iuri Borja <95659045+IuriBorja@users.noreply.github.com>
2023-05-02 21:45:01 +0100

    att

--- Javascript-1-básico/exerc-9-for-loop/script.js
+++ Javascript-1-básico/exerc-9-for-loop/script.js
@@ -10,3 +10,30 @@ for(let i = 1; i <= 20; i++) {
 }
 
 alert(`A tabuada gerada é:\n${mensagem}`);
+
+
+
+
+
+/*-------------------------------*/
+
+
+
+
+const palavra = prompt('Informe uma palavra:');
+let palavraInvertida = '';
+
+for (let i = palavra.length - 1; i >= 0; i--) {
+    palavraInvertida += palavra[i];
+}
+
+if (palavra === palavraInvertida) {
+    alert(`${palavra} é um palíndromo!`);
+} else {
+    alert(`${palavra} não é um palíndromo!
+    \n\n
+    ${palavra} !== ${palavraInvertida}`);
+}
+
+
+
